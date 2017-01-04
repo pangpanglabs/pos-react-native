7ff915116438d1ea558fc85991ee44bead13d836
李澈 <281342000@qq.com>
2017-01-05 00:25:50 +0800

    重新打包

--- ios/pangpangPos/AppDelegate.m
+++ ios/pangpangPos/AppDelegate.m
@@ -30,14 +30,12 @@
   NSURL *jsCodeLocation;
 
   
-#ifdef DEBUG
+  #ifdef DEBUG
     jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
-#else
-  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"bundle/index.ios" withExtension:@"jsbundle"];
-#endif
+  #else
+    jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"bundle/index.ios" withExtension:@"jsbundle"];
+  #endif
   
-
-
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"pangpangPos"
                                                initialProperties:nil
