78b3cd609c694fa05b490c3ef8d03593dd7aeffe
zhou.tong <zhou.tong@elandsystems.com.cn>
2017-02-21 13:29:18 +0800

    删掉版本信息 从sdk中判断版本

--- android/app/src/main/java/com/platform_posrn/PangPangBridge.java
+++ android/app/src/main/java/com/platform_posrn/PangPangBridge.java
@@ -67,10 +67,10 @@ public class PangPangBridge extends ReactContextBaseJavaModule {
             }
 
              params = params.substring(0,params.length()-1);
-             url = "pp:///v1" + path + "?" + params;
+             url = "pp://" + path + "?" + params;
          }
         else{
-             url = "pp:///v1" + path;
+             url = "pp://" + path;
          }
 
         try{
